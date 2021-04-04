82ec124c641ba1e5d5f91dceb7b71ba1beed9ef7
nkhanishvili <nkhanishvili@yahoo.com>
2021-04-04 17:43:18 -0400

    Adding part two

--- MiniProject#Two_PartTwo/script.js
+++ MiniProject#Two_PartTwo/script.js
@@ -1,52 +1,104 @@
 //Global area
-var col = null;
+var div = null;
+var winningPos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
 
 window.onload = function () {
-    //Try and use classname to get a group of divs instead of getting them one by one by id
-    //if you are writing the same type of code multiple times, you are doing it incorrect
 
-    //Attach events using eventlistener here
+    document.addEventListener('click', function (elem) {
 
-    col = document.getElementsByClassName("col");
-    document.getElementById("wrapper").addEventListener('click', function () {
-        userTurn(col);
+        elem = elem || window.event;
+
+        div = elem.target || elem.srcElement;
+        if (div.id == 'reset') {
+
+            ResetGame();
+
+        }
+
+
+        else if (div.className == 'col' && div.innerText == "") {
+
+
+            if (document.getElementById('status').innerText == "Game Over") {
+
+                resetGameBoard();
+
+            } else {
+
+                userTurn();
+
+            }
+
+        }
+
     }, false);
 
-
 }
- 
 
-//function provided. Add parameter(s) if needed
-function userTurn(col) {
-    //Add code here
-    for (var i = 0; i < col.length; i++) {
-        if (col[i].innerText !== "O" && col[i].innerText == "") {
-            col[i].innerText = "X";
-            break;
-        }
+
+
+
+function userTurn() {
+
+    div.innerText = "X";
+
+    if (determineWinner("X")) {
+
+        document.getElementById('message').innerText = "Good Job! You won the game!!!";
+
+    } else {
+        computerTurn();
     }
-     computerTurn(col);
-}
 
- 
-function computerTurn(col) {
- 
-    for (var i = 0; i < col.length; i++) {
-        if (col[i].innerText !== "X" && col[i].innerText == "") {
-            col[i].innerText = "O";
+}
+function computerTurn() {
+
+    var divs = document.getElementsByClassName("col");
+
+    for (var i = 0; i < divs.length; i++) {
+
+        if (divs[i].innerText == "") {
+
+            divs[i].innerText = "O";
+
             break;
+
         }
+
     }
 }
-
 function ResetGame() {
     col = document.getElementsByClassName("col");
     var status = document.getElementById("status").innerHTML = "Game has renewed , good luck!!"
     for (var i = 0; i < col.length; i++) {
 
-        col[i].innerHTML = "";
-        console.log(col[i]);
+        col[i].innerHTML = status;
+
     }
 }
 
 
+function determineWinner(elem) {
+ 
+    var cols = document.getElementsByClassName("col");
+ 
+    for (x = 0; x < winningPos.length; x++) {
+
+        if (cols[winningPos[x][0]].innerText == elem &&
+
+            cols[winningPos[x][1]].innerText == elem &&
+
+            cols[winningPos[x][2]].innerText == elem) {
+ 
+            document.getElementById('status').innerText = "Game Over";
+
+            return true;
+
+        }
+
+    }
+
+    return false;
+
+}
